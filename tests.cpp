e31f48acbdfa25923fa46841349c5980efd38256
Arvid Jonasson <jonassonarvid02@gmail.com>
2023-11-16 19:10:40 +0100

    Reformat code

--- tests.cpp
+++ tests.cpp
@@ -1,14 +1,12 @@
 #define BOOST_TEST_MODULE CurryTest
-#include <boost/test/included/unit_test.hpp>
 #include "curry_function.hpp"
+#include <boost/test/included/unit_test.hpp>
 
 BOOST_AUTO_TEST_CASE(lambda_test)
 {
-    auto add_lambda = [](int a, int b, int c) -> int {
-        return a + b + c;
-    };
+    auto add_lambda = [](int a, int b, int c) -> int { return a + b + c; };
 
-    auto curried_add = curry_function(add_lambda);
+    auto curried_add = currying::curry_function(add_lambda);
 
     BOOST_CHECK_EQUAL(curried_add(10)(11)(12), 10 + 11 + 12);
     BOOST_CHECK_EQUAL(curried_add(10, 11)(12), 10 + 11 + 12);
@@ -19,11 +17,9 @@ BOOST_AUTO_TEST_CASE(lambda_test)
 BOOST_AUTO_TEST_CASE(lambda_capture_test)
 {
     int x = -100;
-    auto add_lambda = [&x](int a, int b, int c) -> int {
-        return a + b + c + x;
-    };
+    auto add_lambda = [&x](int a, int b, int c) -> int { return a + b + c + x; };
 
-    auto curried_add = curry_function(add_lambda);
+    auto curried_add = currying::curry_function(add_lambda);
 
     x = 10;
     BOOST_CHECK_EQUAL(curried_add(10)(11)(12), 10 + 11 + 12 + x);
@@ -35,14 +31,12 @@ BOOST_AUTO_TEST_CASE(lambda_capture_test)
     BOOST_CHECK_EQUAL(curried_add(10, 11, 12), 10 + 11 + 12 + x);
 }
 
-int add_function(int a, int b, int c) {
-    return a + b + c;
-}
+auto add_function(int a, int b, int c) -> int { return a + b + c; }
 
 BOOST_AUTO_TEST_CASE(function_test)
 {
 
-    auto curried_add = curry_function(add_function);
+    auto curried_add = currying::curry_function(add_function);
 
     BOOST_CHECK_EQUAL(curried_add(10)(11)(12), 10 + 11 + 12);
     BOOST_CHECK_EQUAL(curried_add(10, 11)(12), 10 + 11 + 12);
@@ -52,13 +46,11 @@ BOOST_AUTO_TEST_CASE(function_test)
 
 BOOST_AUTO_TEST_CASE(reference_test)
 {
-    auto setter = [](int &set, int val) {
-        set = val;
-    };
+    auto setter = [](int& set, int val) { set = val; };
 
     int x = 5;
 
-    auto curried_add = curry_function(setter);
+    auto curried_add = currying::curry_function(setter);
 
     auto f = curried_add(x);
 
